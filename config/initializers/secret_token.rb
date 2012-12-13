3c545948cc149e530fc27308e52499d79c44285b
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2012-12-13 07:15:37 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vivento::Application.config.secret_token = '25b9925c54d040945f81dffa43ebeb3581881b07df9ce29d8bf4864aaed3e3acb01e5d3328fdd5bbcafb4bc1405c30118ff0707817cfd454ffb6a60d474134fd'
