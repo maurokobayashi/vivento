0b456dd7af7d638ffd4f5026041daa7aecd3b073
Danillo Souza <danillo.fs@gmail.com>
2013-03-24 13:28:09 -0300

    removing condo's relationship with communications'

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -14,9 +14,6 @@
 class Communication < ActiveRecord::Base
   attr_accessible :begin_date, :expiration_date, :message, :subject
 
-  has_many :condo_communications
-  has_many :condos, :through => :condo_communications #relashionship
-
   has_many :building_communications
   has_many :buildings, :through => :building_communications #relashionship
 end
