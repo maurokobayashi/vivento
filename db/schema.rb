fed89fb565d029bbcdc94c96eabaedea8e4ad39c
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2013-03-25 01:21:57 -0300

    adding limit on message column from communication

--- db/schema.rb
+++ db/schema.rb
@@ -48,10 +48,10 @@ ActiveRecord::Schema.define(:version => 20130302223919) do
     t.integer "communication_id"
   end
 
-  create_table "communications", :force => true do |t|
+  create_table "communications",  :force => true do |t|
     t.integer  "condo_id"
-    t.string   "subject"
-    t.string   "message"
+    t.string   "subject",         :limit => 200
+    t.string   "message",         :limit => 2000
     t.date     "begin_date"
     t.date     "expiration_date"
     t.datetime "created_at",      :null => false
